9e5dfb063f3868c0b491f32d7e565ac46e4b2114
activecat <activecat@gmail.com>
2021-05-28 22:42:03 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# dating
+https://web.facebook.com/groups/builddating
+
+I am about to create a dating website primarily in Python, making it open-source and share the source code on GitHub. This will be built together with Python communities around the world. 
+
+Let's join me if you could contribute idea, skill, experience, business perspective, administrator, facilitator, website users etc.
+
+May 2021
---
+++ dating_app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'), ('', 'Unspecified'))
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='')
+
+    birth_date = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=500, null=True, blank=True)
+
+    longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
