a8997877a72ba5dcb5e82a3d8ebbdad2ef50dbec
activecat <activecat@gmail.com>
2021-05-29 13:50:59 +0800

    3rd

--- dating_app/models.py
+++ dating_app/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # https://docs.djangoproject.com/en/3.2/ref/contrib/auth/
 
     GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'), ('', 'Unspecified'))
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='')
