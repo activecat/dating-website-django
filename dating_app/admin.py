e32ed92bb4730dbe55664515f68bad31209f5cf7
activecat <activecat@gmail.com>
2021-06-16 18:51:00 +0800

    user profile photo

--- dating_app/admin.py
+++ dating_app/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-
 from .models import *
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'gender')
+    list_display = ('username', 'name', 'gender', 'age', 'photo')
     ordering = ['user']
 
 
--- dating_app/models.py
+++ dating_app/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import datetime
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # https://docs.djangoproject.com/en/3.2/ref/contrib/auth/
 
-    GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'), ('', 'Unspecified'))
+    GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'), ('', '- - -'))
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='')
 
     birth_date = models.DateField(null=True, blank=True)
@@ -16,8 +17,34 @@ class Profile(models.Model):
     longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
 
+    photo = models.ImageField(upload_to='user_profile_photos', null=True, blank=True)
+
     def __str__(self):
         return self.user.username
+
+    @property
+    def username(self):
+        return self.user.username
+
+    @property
+    def name(self):
+        return "{} {}".format(self.user.first_name, self.user.last_name)
+
+    @property
+    def first_name(self):
+        return self.user.first_name
+
+    @property
+    def last_name(self):
+        return self.user.last_name
+
+    @property
+    def age(self):
+        if self.birth_date:
+            return (datetime.date.today() - self.birth_date).days // 365
+        else:
+            return self.birth_date  # This is None
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -27,5 +54,6 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
 
 # https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
